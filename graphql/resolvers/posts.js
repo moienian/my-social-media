c80d644c283eef319012ba5a53268118f8ce253a
Mohammad Moien <mnmoienian@gmail.com>
2020-10-21 16:17:11 +0330

    add likes resolver

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 const Post = require("../../models/Post");
 const checkAuth = require("../../utils/check-auth");
 
@@ -51,6 +51,22 @@ module.exports = {
       } catch (error) {
         throw new Error(error);
       }
+    },
+    likePost: async (_, { postId }, context) => {
+      const { username } = checkAuth(context);
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
     },
   },
 };
