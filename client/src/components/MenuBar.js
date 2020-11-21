257b82f1ffe6d2a47f0293af9c52bbb04925cafa
Mohammad Moien <mnmoienian@gmail.com>
2020-11-21 09:11:38 +0330

    add Menu bar

---
+++ client/src/components/MenuBar.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
+
+function MenuBar() {
+  const [activeItem, setActiveItem] = useState("home");
+
+  const handleItemClick = (e, { name }) => setActiveItem(name);
+
+  return (
+    <Menu pointing secondary color="teal" size="massive">
+      <Menu.Item
+        name="home"
+        active={activeItem === "home"}
+        onClick={handleItemClick}
+        as={Link}
+        to="/"
+      />
+      <Menu.Menu position="right">
+        <Menu.Item
+          name="login"
+          active={activeItem === "login"}
+          onClick={handleItemClick}
+          as={Link}
+          to="/login"
+        />
+        <Menu.Item
+          name="register"
+          active={activeItem === "register"}
+          onClick={handleItemClick}
+          as={Link}
+          to="/register"
+        />
+      </Menu.Menu>
+    </Menu>
+  );
+}
+
+export default MenuBar;
