f96d3ce249e5539ceb4aa20ca16edb09b0ad7880
Mohammad Moien <mnmoienian@gmail.com>
2020-11-29 10:38:31 +0330

    handle creat new post

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useQuery } from "@apollo/react-hooks";
-import gql from "graphql-tag";
 import { Grid } from "semantic-ui-react";
+import { AuthContext } from "../context/auth";
 import PostCard from "../components/PostCard";
+import PostForm from "../components/PostForm";
+import { FETCH_POST_QUERY } from "../utils/graphql";
 
 function Home() {
+  const { user } = useContext(AuthContext);
   const { loading, data } = useQuery(FETCH_POST_QUERY);
 
   return (
@@ -13,6 +16,11 @@ function Home() {
         <h1>Recent Posts</h1>
       </Grid.Row>
       <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm />
+          </Grid.Column>
+        )}
         {loading ? (
           <h1>Loading Posts ...</h1>
         ) : (
@@ -27,27 +35,5 @@ function Home() {
     </Grid>
   );
 }
-
-const FETCH_POST_QUERY = gql`
-  {
-    getPosts {
-      id
-      body
-      createdAt
-      username
-      likeCount
-      likes {
-        username
-      }
-      commentCount
-      comments {
-        id
-        username
-        createdAt
-        body
-      }
-    }
-  }
-`;
 
 export default Home;
