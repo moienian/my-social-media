263d6d258534fd134dbf28b84a157b7f7f038d68
Mohammad Moien <mnmoienian@gmail.com>
2020-11-30 10:48:42 +0330

    handle error on sending empty post

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -10,7 +10,7 @@ const PostForm = () => {
     body: "",
   });
 
-  const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
+  const [createPost, { loading, error }] = useMutation(CREATE_POST_MUTATION, {
     variables: values,
     update(proxy, result) {
       const data = proxy.readQuery({
@@ -29,26 +29,39 @@ const PostForm = () => {
       });
       values.body = "";
     },
+    onError(err) {
+      console.log(err.graphQLErrors[0].message);
+    },
   });
 
   function createPostCallback() {
     createPost();
   }
   return (
-    <Form onSubmit={onSubmit}>
-      <h2>Create a post:</h2>
-      <Form.Field>
-        <Form.Input
-          placeholder="Hi World"
-          name="body"
-          onChange={onChange}
-          value={values.body}
-        />
-        <Button type="submit" color="teal">
-          Submit
-        </Button>
-      </Form.Field>
-    </Form>
+    <>
+      <Form onSubmit={onSubmit} className={loading ? "loading" : ""}>
+        <h2>Create a post:</h2>
+        <Form.Field>
+          <Form.Input
+            placeholder="Hi World"
+            name="body"
+            onChange={onChange}
+            value={values.body}
+            error={error ? true : false}
+          />
+          <Button type="submit" color="teal">
+            Submit
+          </Button>
+        </Form.Field>
+      </Form>
+      {error && (
+        <div className="ui error message" style={{ marginBottom: 20 }}>
+          <ul className="list">
+            <li>{error.graphQLErrors[0].message}</li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 };
 
